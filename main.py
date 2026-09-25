0c96be69462cec426c20395f2db6e8f77dd09ade
Luiz Otávio Borella e Souza <luiz.o.souza6@edu.senai.br>
2026-09-25 08:07:15 -0300

    Final Version

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, send_file
 import fdb
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from fpdf import FPDF
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'chave_secreta_da_turma_b'
@@ -209,6 +209,64 @@ def login():
 def logout():
     session.pop('id_usuario', None)
     return redirect(url_for('index'))
+
+@app.route('/livros/relatorio', methods=['GET'])
+def relatorio():
+
+    cursor = con.cursor()
+
+    cursor.execute("""
+        SELECT id_livro, nome, autor, ano_publicado
+        FROM livro
+    """)
+
+    livros = cursor.fetchall()
+    cursor.close()
+
+    pdf = FPDF()
+    pdf.set_auto_page_break(auto=True, margin=15)
+    pdf.add_page()
+
+    pdf.set_font("Arial", style='B', size=16)
+    pdf.cell(200, 10, "Relatório de Livros", ln=True, align='C')
+
+    pdf.ln(5)  # Espaço entre o título e a linha
+    pdf.line(10, pdf.get_y(), 200, pdf.get_y())  # Linha abaixo do título
+    pdf.ln(5)  # Espaço após a linha
+
+    pdf.set_font("Arial", size=12)
+
+    for livro in livros:
+        pdf.cell(
+            200,
+            10,
+            f"ID: {livro[0]} - {livro[1]} - {livro[2]} - {livro[3]}",
+            ln=True
+        )
+
+    contador_livros = len(livros)
+
+    pdf.ln(10)  # Espaço antes do contador
+
+    pdf.set_font("Arial", style='B', size=12)
+
+    pdf.cell(
+        200,
+        10,
+        f"Total de livros cadastrados: {contador_livros}",
+        ln=True,
+        align='C'
+    )
+
+    pdf_path = "relatorio_livros.pdf"
+
+    pdf.output(pdf_path)
+
+    return send_file(
+        pdf_path,
+        as_attachment=True,
+        mimetype='application/pdf'
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
